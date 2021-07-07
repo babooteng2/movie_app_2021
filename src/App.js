e816cd133ad5389700e34b09b887e98322341314
TomKim <babooteng2@gmail.com>
2021-07-07 18:52:47 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,48 @@
-function Food({name, image}) {  
-  return (
-    <div>
-      <h1>we love {name}</h1>
-      <a href={image}><img src={image} alt={name}/></a>      
-    </div>
-  ); 
-}
+
+import PropTypes from "prop-types"
 
 const foodILike = [
   {
     id:0,
     name: "Kimbab",    
-    image: "http://placeimg.com/200/100/any"
+    image: "http://placeimg.com/200/100/any/1",
+    rating: 4.2
   },
   {
     id:1,
     name: "Dotori",    
-    image: "http://placeimg.com/200/100/any"
+    image: "http://placeimg.com/200/100/any/2",
+    rating: 4.1
   },
   {
     id:2,
     name: "Chukumi",    
-    image: "http://placeimg.com/200/100/any"
+    image: "http://placeimg.com/200/100/any/3",
+    rating: 4.3
   },
   {
     id:3,
     name: "Donkasu",    
-    image: "http://placeimg.com/200/100/any"
+    image: "http://placeimg.com/200/100/any/4",
+    rating: 5.0
   }
 ];
+
+function Food({name, image, rating}) {  
+  return (
+    <div>
+      <h1>we love {name}</h1>
+      <a href={image}><img src={image} alt={name}/></a>
+      <h4>rating: {rating}/5.0</h4>
+    </div>
+  ); 
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+}
 
 function App() {
   return (
@@ -36,7 +50,8 @@ function App() {
       {foodILike.map(dish => (
         <Food key  = {dish.id}
               name = {dish.name} 
-              image= {dish.image}/>
+              image= {dish.image}
+              rating={dish.rating}/>
       ))}      
     </div>
   );
