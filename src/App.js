d4a98e96ab949bd78e19f4a3dbb40b1275396668
TomKim <babooteng2@gmail.com>
2021-07-10 13:57:03 +0900

    #6.2 Buildig the Navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+//import { BrowserRouter, Route } from "react-router-dom";
+// HashRouter를 사용하면 #표시가 브라우저 주소창에 남지만 git page에 업로드 설정하기 쉽다.
 import { HashRouter, Route } from "react-router-dom";
+import Navigation from "./components/Navigation"
 import Home from "./routes/Home"
 import About from "./routes/About"
 
 function App() {
   return <HashRouter>
+    <Navigation/>
     <Route path="/" exact={true} component={Home}/>
     <Route path="/about" component={About}/>    
   </HashRouter>
